796a9c85ac840650608dc9b3de7eb8508058bd1d
Leober Ramos <leober.ramos33@gmail.com>
2021-02-22 16:39:25 -0400

    fix: 12 hours format in generated at date.

--- app/Bot.php
+++ app/Bot.php
@@ -67,7 +67,7 @@ class Bot
             $spreadsheetData["accounts"]
         );
         $spreadsheetData["accounts"] = $platformsData;
-        $spreadsheetData["generatedAt"] = date("H:i:s A");
+        $spreadsheetData["generatedAt"] = date("h:i:s A");
 
         $output = json_encode($spreadsheetData);
         $outputFilename = $this->outputPath . "/" . date("d-m-Y") . ".json";
